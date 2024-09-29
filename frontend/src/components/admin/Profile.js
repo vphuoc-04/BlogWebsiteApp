67e6cb821430b72245e6ee0bd554689b5f696cfa
vphuoc-04 <vanphuoc240904@gmail.com>
2024-09-29 10:42:26 +0700

    add admin avatar

--- frontend/src/components/admin/Profile.js
+++ frontend/src/components/admin/Profile.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { AdminContext } from '../../context/AuthContext';
 import { AdminProfile } from '../../core/admin/AdminProfile';
+import { AdminData } from '../../data/AdminData';
 
 const Profile = () => {
+    const { currentAdmin } = useContext(AdminContext)
+    const [admin, setAdmin] = AdminData(currentAdmin);
+
     return (
-        <AdminProfile />
+        <AdminProfile 
+            admin = { admin }
+        />
     )
 }
 
--- frontend/src/core/admin/AdminProfile.js
+++ frontend/src/core/admin/AdminProfile.js
@@ -1,7 +1,20 @@
-const AdminProfile = () => {
+import { FaCamera } from 'react-icons/fa';
+
+const AdminProfile = ({
+    admin,
+}) => {
     return (
         <div className = "AdminProfile">
-            Profile
+            <div className = "Avatar">
+                <label className = "AvatarDisplay">
+                    <div className = "AvatarImage">
+                        <img src = { admin.avatar } alt = "" />
+                    </div>
+                    <div className = "CameraIcon">
+                        <FaCamera />
+                    </div>
+                </label>
+            </div>
         </div>
     )
 }
