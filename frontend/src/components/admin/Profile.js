f9719b65bf3d20ce0d86169de0a597c3c07c0f98
vphuoc-04 <vanphuoc240904@gmail.com>
2024-09-29 16:02:38 +0700

    add view admin avatar

--- frontend/src/components/admin/Profile.js
+++ frontend/src/components/admin/Profile.js
@@ -6,10 +6,22 @@ import { AdminData } from '../../data/AdminData';
 const Profile = () => {
     const { currentAdmin } = useContext(AdminContext)
     const [admin, setAdmin] = AdminData(currentAdmin);
+    const [avatarAction, setAvatarAction] = useState(null);
+    const [avatarView, setAvatarView] = useState(null);
+
+    // Admin avatar
+    const HandleAvatarActionSelect = () => { setAvatarAction(Show => !Show); }
+    const HandleAvatarView = () => { setAvatarView(true); setAvatarAction(false); }
+    const HandleCloseAvatarView = () => { setAvatarView(false); }
 
     return (
         <AdminProfile 
             admin = { admin }
+            avatarAction = { avatarAction }
+            HandleAvatarActionSelect = { HandleAvatarActionSelect }
+            avatarView = { avatarView }
+            HandleAvatarView = { HandleAvatarView }
+            HandleCloseAvatarView = { HandleCloseAvatarView }
         />
     )
 }
--- frontend/src/core/admin/AdminProfile.js
+++ frontend/src/core/admin/AdminProfile.js
@@ -2,11 +2,18 @@ import { FaCamera } from 'react-icons/fa';
 
 const AdminProfile = ({
     admin,
+
+    // Admin avatar
+    avatarAction,
+    HandleAvatarActionSelect,
+    avatarView,
+    HandleAvatarView,
+    HandleCloseAvatarView
 }) => {
     return (
         <div className = "AdminProfile">
             <div className = "Avatar">
-                <label className = "AvatarDisplay">
+                <label className = "AvatarDisplay" onClick = { HandleAvatarActionSelect }>
                     <div className = "AvatarImage">
                         <img src = { admin.avatar } alt = "" />
                     </div>
@@ -14,7 +21,28 @@ const AdminProfile = ({
                         <FaCamera />
                     </div>
                 </label>
+                {avatarAction && (
+                    <div className = "AvatarActions">
+                        <div className = "button" onClick = { HandleAvatarView }>
+                            <i className = "fa-regular fa-user"></i>&nbsp;View avatar picture
+                        </div>
+                    </div>
+                )}
             </div>
+            {avatarView && <div className = "overlay"></div>}
+            {avatarView && (
+                <div className = "AvatarView">
+                    <div className = "CloseViewAvatar" onClick = { HandleCloseAvatarView }>&times;</div>
+                    <div className = "ActionViewAvatar">
+                        <i 
+                            className = "fa-solid fa-ellipsis-vertical" 
+                        ></i>
+                    </div>
+                    <div className = "Avatar">
+                        <img src = { admin.avatar } alt = "" />
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
